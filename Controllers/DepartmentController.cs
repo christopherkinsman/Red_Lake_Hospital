e66ee695b251e95c7d552e28330dbe29e879daec
Chris Kinsman <chris.t.kinsman@gmail.com>
2021-04-30 17:31:35 -0400

    MVP

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using Red_Lake_Hospital_Redesign_Team6.Models;
+using Red_Lake_Hospital_Redesign_Team6.Models.ViewModels;
+using System.Net.Http.Headers;
+using System.Web.Script.Serialization;
+
+namespace Red_Lake_Hospital_Redesign_Team6.Controllers
+{
+    public class DepartmentController : Controller
+    {
+
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+        static DepartmentController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44349//api/");
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /// <summary>
+        /// Lists all the departments in the database
+        /// </summary>
+        /// <returns>
+        ///     If successfull in retreiving the data from the database, then returns the data to the view
+        ///     If not -> Calls the Error Action to invoke the error message.
+        /// </returns>
+        /// <example>
+        /// GET: Department/ListDepartments
+        /// </example>
+        public ActionResult ListDepartments()
+        {
+            string url = "DepartmentData/ListDepartments";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<DepartmentsDto> ListOfDepartments = response.Content.ReadAsAsync<IEnumerable<DepartmentsDto>>().Result;
+                return View(ListOfDepartments);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        /// <summary>
+        ///  Renders the view for creating a new Department
+        /// </summary>
+        /// <returns>Returns a view for creating a new Department</returns>
+        /// <example>
+        /// GET : Department/Create
+        /// </example>
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Creates a department in the database
+        /// </summary>
+        /// <param name="DepartmentInfo">Information on the new department to be entered into the database.</param>
+        /// <returns>
+        ///     If successfully connected to the api, the new department would be created in the database, and the control
+        ///     is redirected to the action "ListDepartments" which displayes all the departments in the database
+        ///     If not successfull , returns an error.
+        /// </returns>
+        /// <example>
+        ///     POST : Department/Create/{FORM Data}
+        /// </example>
+
+        [HttpPost]
+        public ActionResult Create(DepartmentsModel DepartmentInfo)
+        {
+            string url = "DepartmentData/AddDepartment";
+            HttpContent content = new StringContent(jss.Serialize(DepartmentInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ListDepartments");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        /// <summary>
+        /// Renders the view to update a Department
+        /// </summary>
+        /// <param name="id">Id of the department to be updated</param>
+        /// <returns>
+        ///     If successfull , returns a view which contains details about the department 
+        ///     If not successfull , returns an error.
+        /// </returns>
+        /// <example>
+        ///     GET: Department/Update/1
+        /// </example>
+
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            string url = "DepartmentData/FindDepartment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                DepartmentsDto UpdatedDepartment = response.Content.ReadAsAsync<DepartmentsDto>().Result;
+                return View(UpdatedDepartment);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        /// <summary>
+        /// Updates the information of a department in the database.
+        /// </summary>
+        /// <param name="DepartmentInfo">DepartmentsModel object which contains information of the department</param>
+        /// <returns>
+        ///     If connection to the api is successfull, updates the contents of the existing department and the control is 
+        ///     redirected to the Action "ListDepartments" which renders the view to display all the departments
+        ///     If not successfull, then an error message is thrown. 
+        /// </returns>
+        /// <example>
+        ///     POST: Department/Update/2/{Form Data}
+        /// </example>
+
+        [HttpPost]
+        public ActionResult Update(int id, DepartmentsModel DepartmentInfo)
+        {
+            string url = "DepartmentData/UpdateDepartment/" + id;
+            HttpContent content = new StringContent(jss.Serialize(DepartmentInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ListDepartments");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        /// <summary>
+        /// Asks the user to confirm before deleting the department from the database permanently. Shows the information about the department   
+        /// </summary>
+        /// <param name="id">Id of the Department to be deleted.</param>
+        /// <returns>
+        ///     If successfull, renders the details of the department to be deleted to the corresponding View.
+        ///     If not successfull, then returns an error.
+        /// </returns>
+        /// <example>
+        ///     GET: Department/DeleteConfirm/5
+        /// </example>
+
+        [HttpGet]
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "DepartmentData/FindDepartment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                DepartmentsDto Department = response.Content.ReadAsAsync<DepartmentsDto>().Result;
+                return View(Department);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        /// <summary>
+        ///     Deletes the department from the database
+        /// </summary>
+        /// <param name="id">Id of the Department to be deleted.</param>
+        /// <returns>
+        ///     If successfull, deletes the testimonial from the database  and the control is 
+        ///     redirected to the Action "ListDepartments" which renders the view to display all the departments
+        /// </returns>
+        /// <example>
+        ///     POST: Department/Delete/5
+        /// </example>
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            string url = "DepartmentData/DeleteDepartment/" + id;
+            HttpContent content = new StringContent("");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ListDepartments");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        /// <summary>
+        /// Displays the details of the department including the testimonials for the department
+        /// </summary>
+        /// <param name="Id">Id of the department</param>
+        /// <returns>
+        ///     If successfull in fetching the data from the api controller, returns a "DepartmentDetails" View Model
+        ///     which contains information on the details of the department and the testimonials of the department.
+        /// </returns>
+        /// <example>
+        ///     GET: Department/DetailsofDepartment/5
+        /// </example>
+
+        public ActionResult DetailsofDepartment(int Id)
+        {
+            DepartmentDetails departmentdetails = new DepartmentDetails();
+            string url = "DepartmentData/FindDepartment/" + Id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                DepartmentsDto department = response.Content.ReadAsAsync<DepartmentsDto>().Result;
+                departmentdetails.DepartmentDto = department;
+
+                url = "DepartmentData/FindTestimonialsForDepartment/" + Id;
+                response = client.GetAsync(url).Result;
+                IEnumerable<TestimonialDto> testimonials = response.Content.ReadAsAsync<IEnumerable<TestimonialDto>>().Result;
+                departmentdetails.Testimonials = testimonials;
+                return View(departmentdetails);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        /// <summary>
+        /// Renders the view to display an error 
+        /// </summary>
+        /// <returns>
+        /// </returns>
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/TestimonialDataController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Red_Lake_Hospital_Redesign_Team6.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace Red_Lake_Hospital_Redesign_Team6.Controllers
+{
+    public class TestimonialDataController : ApiController
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// Finds the details of the testimonial in the database
+        /// </summary>
+        /// <param name="id">Id of the testimonial to be found</param>
+        /// <returns>
+        ///     Returns a testimonial object which contains information of the testimonial
+        /// </returns>
+        /// <example>
+        ///     GET : api/TestimonialData/FindTestimonial/1
+        /// </example>
+
+        [HttpGet]
+        [ResponseType(typeof(TestimonialDto))]
+        public IHttpActionResult FindTestimonial(int id)
+        {
+
+            Testimonial testimonial = db.Testimonials.Find(id);
+            if (testimonial == null)
+            {
+                return NotFound();
+            }
+            TestimonialDto NewTestimonial = new TestimonialDto
+            {
+                testimonial_Id = testimonial.testimonial_Id,
+                testimonial_Content = testimonial.testimonial_Content,
+                first_Name = testimonial.first_Name,
+                last_Name = testimonial.last_Name,
+                email = testimonial.email,
+                phone_Number = testimonial.phone_Number,
+                Has_Pic = testimonial.Has_Pic,
+                Pic_Extension = testimonial.Pic_Extension,
+                posted_Date = testimonial.posted_Date,
+                Approved = testimonial.Approved
+            };
+            return Ok(NewTestimonial);
+        }
+
+        /// curl -H "Content-Type:application/json" -d @NewTestimonial.json "https://localhost:44349/api/TestimonialData/AddTestimonial/2"
+        /// <summary>
+        /// Adds a testimonial to a department 
+        /// </summary>
+        /// <param name="Testimonial">A testimonial object.Sent as POST form data</param>
+        /// <param name="DepartmentId">Id of the department to which the testimonial is to be added</param>
+        /// <returns>status code 200 if successful. 400 if unsuccessful</returns>
+        /// <example>
+        /// POST: api/TestimonialData/AddTestimonial/2
+        /// FORM DATA: Testimonial JSON Object
+        /// </example>
+
+        [HttpPost]
+        [ResponseType(typeof(Testimonial))]
+        [Route("api/TestimonialData/AddTestimonial/{DepartmentId}")]
+        public IHttpActionResult AddTestimonial(int DepartmentId, Testimonial Testimonial)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            DepartmentsModel DepartmentSelected = db.Departments.Find(DepartmentId);
+
+            if (DepartmentSelected == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                DepartmentSelected.Testimonials = new List<Testimonial>();
+                db.Testimonials.Add(Testimonial);
+                DepartmentSelected.Testimonials.Add(Testimonial);
+                db.SaveChanges();
+                return Ok(Testimonial.testimonial_Id);
+            }
+        }
+
+        /// <summary>
+        ///     Updates the information of the testimonial in the database
+        /// </summary>
+        /// <param name="DepartmentId">Id of the department</param>
+        /// <param name="TestimonialId">Id of the testimonial</param>
+        /// <param name="Testimonial">Testimonial object which contains the new information of the testimonial</param>
+        /// <returns></returns>
+        /// <example>
+        ///     POST: api/TestimonialData/UpdateTestimonial/2/3
+        /// </example>
+
+        [HttpPost]
+        [ResponseType(typeof(void))]
+        [Route("api/TestimonialData/UpdateTestimonial/{DepartmentId}/{TestimonialId}")]
+        public IHttpActionResult UpdateTestimonial(int DepartmentId, int TestimonialId, [FromBody] Testimonial Testimonial)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (TestimonialId != Testimonial.testimonial_Id)
+            {
+                return BadRequest();
+            }
+
+            DepartmentsModel DepartmentSelected = db.Departments.Find(DepartmentId);
+
+            if (DepartmentSelected == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                db.Entry(Testimonial).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TestimonialExists(TestimonialId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+
+        }
+
+        /// <summary>
+        /// Deletes a testimonial from the database
+        /// </summary>
+        /// <param name="id">Id of the testimonial to be deleted</param>
+        /// <returns>
+        ///     Testimonial object which contains the information of the testimonial which was deleted.
+        /// </returns>
+        /// <example>
+        ///     POST: api/TestimonialData/DeleteTestimonial/2
+        /// </example>
+
+        [HttpPost]
+        [ResponseType(typeof(DepartmentsModel))]
+        public IHttpActionResult DeleteTestimonial(int id)
+        {
+            Testimonial testimonial = db.Testimonials.Find(id);
+
+            if (testimonial == null)
+            {
+                return NotFound();
+            }
+
+            db.Testimonials.Remove(testimonial);
+            db.SaveChanges();
+
+            return Ok(testimonial);
+
+        }
+
+        /// <summary>
+        ///     Checks if the Testimonial is present in the database.
+        /// </summary>
+        /// <param name="id">Id of the testimonial</param>
+        /// <returns>
+        ///     Boolean 1, if the testimonial is present in the database
+        ///     Boolean 0, if the testimonial is not present in the database.
+        /// </returns>
+
+        private bool TestimonialExists(int id)
+        {
+            return db.Testimonials.Count(e => e.testimonial_Id == id) > 0;
+        }
+    }
+
+}
